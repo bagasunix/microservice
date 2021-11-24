209f8cd8ea9d2abeb3e9f4803cc3b4c84258986c
Aldino Pratama <aldinopratama15@gmail.com>
2021-11-24 10:10:46 +0700

    set secure gateway

--- gateway/app.js
+++ gateway/app.js
@@ -2,25 +2,37 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const csrf = require('csurf')
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 // setup route middlewares
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!",
+});
 const hpp = require('hpp');
-const csrfProtection = csrf({ cookie: true })
-const parseForm = bodyParser.urlencoded({ extended: false })
-
 const app = express();
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(logger("dev"));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(hpp());
+app.use(helmet());
+app.use(xss());
+app.use("/", limiter);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
